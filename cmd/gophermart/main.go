492c3793063ccae6836478755a3b5c00d7474e8a
Sviatoslav Tregubov <tregubov.sv@yandex.ru>
2025-07-29 20:00:18 +0300

    start

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		return
+	}
+}
