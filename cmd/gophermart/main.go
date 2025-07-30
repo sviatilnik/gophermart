bce8dbf71698a40719e88bd1a05d039028257144
Sviatoslav Tregubov <tregubov.sv@yandex.ru>
2025-07-30 09:11:02 +0300

    gracefull shutdown

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/sviatilnik/gophermart/internal/config"
 	"go.uber.org/zap"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-
 	conf := getConfig()
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
@@ -21,8 +27,29 @@ func main() {
 		logger.Info(r.RequestURI)
 	})
 
-	err := http.ListenAndServe(conf.Host, r)
-	if err != nil {
+	quitChan := make(chan os.Signal, 1)
+	signal.Notify(quitChan, syscall.SIGINT, syscall.SIGTERM)
+
+	server := &http.Server{
+		Addr:    conf.Host,
+		Handler: r,
+	}
+
+	go func() {
+		logger.Info(fmt.Sprintf("start server on %s", server.Addr))
+		err := server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Fatal(err.Error())
+		}
+	}()
+
+	<-quitChan
+
+	logger.Info("shutting down server...")
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
 		logger.Fatal(err.Error())
 	}
 }
